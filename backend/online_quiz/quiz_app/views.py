Fix result calculation logic.

backend/online_quiz/quiz_app/views.py
@@ -3,8 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import QuizGetAllSerializer, QuestionGetSelectedSerializer, ResultGetSelectedSerializer
 from .models import  Option, Question, Quiz, Result, Submission
-import numpy
-import json
+import numpy as np
 
 # Returns all quiz which includes quiz descriptions as text.
 @api_view(['GET'])
@@ -23,15 +22,14 @@ def getQuestion(request, pk):
 # ###
 @api_view(['POST'])
 def submitAnswers(request, pk):
-    data = json.loads(request.data)
-    selected_options_ids = data['selected_options']
+    selected_options_ids = request.data['selected_options']
     selected_quiz = Quiz.objects.get(id=pk)
-    result_factors = numpy.zeros(len(selected_quiz.results))
+    result_factors = np.zeros(selected_quiz.results.count())
     # TODO(MBM): "selected_options" and "selected_quiz.result" length must be same. Return error if it is not.
     for selected_options_id in selected_options_ids:
         selected_option = Option.objects.get(id=selected_options_id)
         result_factors += selected_option.result_factors
-    result_index = result_factors.index(max(result_factors))
-    result = selected_quiz.results[result_index]
+    result_index = np.argmax(result_factors)
+    result = selected_quiz.results.all()[int(result_index)]
     serializer = ResultGetSelectedSerializer(result)
     return Response(serializer.data, status=status.HTTP_200_OK)