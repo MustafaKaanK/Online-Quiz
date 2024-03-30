5b361422247f99e461f201ca6b90d82160886800
bahadirmaktav <bahadirmaktavv@gmail.com>
2024-03-30 17:32:53 +0300

    Improve error handling logic for api calls.

--- backend/online_quiz/quiz_app/views.py
+++ backend/online_quiz/quiz_app/views.py
@@ -17,41 +17,71 @@ def getQuizzes(request):
 # .../quizzes/<str:pk>/
 @api_view(['GET'])
 def getQuiz(request, pk):
-    quiz = Quiz.objects.get(id=pk)
-    serializer = QuizDetailedSerializer(quiz)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    try:
+        quiz = Quiz.objects.get(id=pk)
+        serializer = QuizDetailedSerializer(quiz)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except Quiz.DoesNotExist:
+        return Response({"error": f"Quiz with id {pk} does not exist"}, status=status.HTTP_404_NOT_FOUND)
 
 # TODO(MBM): Add comment here.
 # .../quizzes/<str:pk>/questions/
 @api_view(['GET'])
 def getQuestions(request, pk):
-    quiz = Quiz.objects.get(id=pk)
-    questions = quiz.questions.all()
-    serializer = QuestionSimplifiedSerializer(questions, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    try:
+        quiz = Quiz.objects.get(id=pk)
+        questions = quiz.questions.all()
+        serializer = QuestionSimplifiedSerializer(questions, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except Quiz.DoesNotExist:
+        return Response({"error": f"Quiz with id {pk} does not exist"}, status=status.HTTP_404_NOT_FOUND)
 
 # TODO(MBM): Add comment here.
 # .../quizzes/<str:pk>/questions/<str:pk>/
 @api_view(['GET'])
 def getQuestion(request, pk1, pk2):
-    quiz = Quiz.objects.get(id=pk1)
-    question = quiz.questions.get(number_order=pk2)
-    serializer = QuestionDetailedSerializer(question)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    try:
+        quiz = Quiz.objects.get(id=pk1)
+        question = quiz.questions.get(number_order=pk2)
+        serializer = QuestionDetailedSerializer(question)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except Quiz.DoesNotExist:
+        return Response({"error": f"Quiz with id {pk1} does not exist!"}, status=status.HTTP_404_NOT_FOUND)
+    except Question.DoesNotExist:
+        return Response({"error": f"Question with number {pk2} does not exist!"}, status=status.HTTP_404_NOT_FOUND)
 
 # TODO(MBM): Add comment here.
 # .../quizzes/<str:pk>/submission/
 @api_view(['POST'])
 def submitAnswers(request, pk):
-    selected_options_nums = request.data['selected_options']
-    selected_quiz = Quiz.objects.get(id=pk)
-    result_factor_list = np.zeros(selected_quiz.results.count())
-    # TODO(MBM): "selected_options" and "selected_quiz.result" length must be same. Return error if it is not.
-    for i in range(1, len(selected_options_nums) + 1):
-        selected_question = selected_quiz.questions.get(number_order=i)
-        selected_option = selected_question.options.get(number_order=selected_options_nums[i - 1])
-        result_factor_list += selected_option.result_factor_list
-    result_index = np.argmax(result_factor_list)
-    result = selected_quiz.results.all().order_by('id')[int(result_index)]
-    serializer = ResultDetailedSerializer(result)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    try:
+        selected_options_nums = request.data['selected_options']
+        selected_options_nums_size = len(selected_options_nums)
+        selected_quiz = Quiz.objects.get(id=pk)
+        selected_quiz_results_size = selected_quiz.results.count()
+        if selected_options_nums_size != selected_quiz.number_of_questions:
+            raise ValueError("Size of selected_options does not match with the number of questions in selected quiz!")
+        result_factor_list = np.zeros(selected_quiz_results_size)
+        for i in range(1, selected_options_nums_size + 1):
+            try:
+                selected_question = selected_quiz.questions.get(number_order=i)
+                selected_option = selected_question.options.get(number_order=selected_options_nums[i - 1])
+                if len(selected_option.result_factor_list) != selected_quiz_results_size:
+                    raise ValueError("Size of result factor list for selected option does not match with the selected quiz results size!")
+                result_factor_list += selected_option.result_factor_list
+            except Question.DoesNotExist:
+                return Response({"error": f"Question with number {i} does not exist!"}, status=status.HTTP_404_NOT_FOUND)
+            except Option.DoesNotExist:
+                return Response({"error": f"Quiz with number {selected_options_nums[i - 1]} does not exist!"}, status=status.HTTP_404_NOT_FOUND)
+            except ValueError as e:
+                return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        result_index = np.argmax(result_factor_list)
+        result = selected_quiz.results.all().order_by('id')[int(result_index)]
+        serializer = ResultDetailedSerializer(result)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except KeyError:
+        return Response({"error": "'selected_options' not found in request data!"}, status=status.HTTP_400_BAD_REQUEST)
+    except Quiz.DoesNotExist:
+        return Response({"error": f"Quiz with id {pk} does not exist!"}, status=status.HTTP_404_NOT_FOUND)
+    except ValueError as e:
+        return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
